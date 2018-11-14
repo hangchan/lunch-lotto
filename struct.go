5eed113be8fbd1e70bc4d57edcbd2716b034800b
Hang Chan <hang@needsystem.com>
2018-11-14 15:09:26 -0500

    check in

--- struct.go
+++ struct.go
@@ -1,37 +1,63 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-type pair struct{
-	name1 string
-	name2 string
+type pair struct {
+	name1     string
+	name2     string
+	day       int
+	scheduled bool
 }
 
 func main() {
 	people := []string{"John", "Jane", "Jim", "Steve", "Bill"}
-	// get a list of pairs
-	// pairs := makePairs(people)
-	// schedule day for pairs
-	//scheduleDay(pairs)
-	printPairs(people)
+	peopleMap := makePeopleMap(people)
+
+	pairs := makePairs(people)
+	day := 1
+	for i, v := range pairs {
+		day = 1
+		peopleMap = makePeopleMap(people)
+		for ii, _ := range peopleMap {
+			if v.scheduled == false && (ii == v.name1 || ii == v.name2) {
+				v.day = day
+				v.scheduled = true
+				peopleMap[v.name1] = true
+				peopleMap[v.name2] = true
+			}
+			fmt.Printf("index:%4v\tv.name1:%8v\tv.name2:%8v\tv.day:%4v\tv.scheduled:%8v\n", i, v.name1, v.name2, v.day, v.scheduled)
+			day++
+		}
+		fmt.Printf("index:%4v\tv.name1:%8v\tv.name2:%8v\tv.day:%4v\tv.scheduled:%8v\n", i, v.name1, v.name2, v.day, v.scheduled)
+	}
 }
 
 func newPair(first string, second string) pair {
-		var p pair
-		if first != second {
-			p = pair{name1: first, name2: second}
-			return p
-		}
+	var p pair
+	if first != second {
+		p = pair{name1: first, name2: second}
 		return p
+	}
+	return p
 }
 
-func printPairs (people []string) {
+func makePairs(people []string) []pair {
+	var pairs []pair
 	for i, _ := range people {
-		for j := i+1; j < len(people); j++ {
+		for j := i + 1; j < len(people); j++ {
 			p := newPair(people[i], people[j])
-			fmt.Printf("Pair:\t%v\n", p)
+			//fmt.Printf("name1:%8v\tname2:%8v\n", p.name1, p.name2)
+			pairs = append(pairs, p)
 		}
 	}
+	return pairs
+}
+
+func makePeopleMap(people []string) map[string]bool {
+	// make peopleMap
+	peopleMap := make(map[string]bool)
+	for _, v := range people {
+		peopleMap[v] = false
+	}
+	return peopleMap
 }
