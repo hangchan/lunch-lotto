de45982768491bba6fa79d48405ca1f962f531eb
Hang Chan <hang@needsystem.com>
2018-11-14 21:36:51 -0500

    added sort

--- struct.go
+++ struct.go
@@ -1,36 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 type pair struct {
+	day       int
 	name1     string
 	name2     string
-	day       int
 	scheduled bool
 }
 
 func main() {
-	people := []string{"John", "Jane", "Jim", "Bill"}
+	people := []string{"John", "Jane", "Jim", "Bill", "Joe"}
 	pairs := makePairs(people)
 
 	scheduleDay(pairs, people)
-	for i, v := range pairs {
-		fmt.Println(i, v)
+	sort.Slice(pairs, func(i, j int) bool {
+		return pairs[i].day < pairs[j].day
+	})
+	for _, v := range pairs {
+		fmt.Printf("Day: %v\t Pair: %v-%v\t\n", v.day, v.name1, v.name2)
 	}
 
 }
 
 func newPair(first string, second string) pair {
 	var p pair
+
 	if first != second {
 		p = pair{name1: first, name2: second}
 		return p
 	}
+
 	return p
 }
 
 func makePairs(people []string) []pair {
 	var pairs []pair
+
 	for i, _ := range people {
 		for j := i + 1; j < len(people); j++ {
 			p := newPair(people[i], people[j])
@@ -38,15 +47,18 @@ func makePairs(people []string) []pair {
 			pairs = append(pairs, p)
 		}
 	}
+
 	return pairs
 }
 
 func makePeopleMap(people []string) map[string]bool {
 	// make peopleMap
 	peopleMap := make(map[string]bool)
+
 	for _, v := range people {
 		peopleMap[v] = false
 	}
+
 	return peopleMap
 }
 
@@ -59,7 +71,7 @@ func scheduleDay(pairs []pair, people []string) []pair {
 		peopleMap = makePeopleMap(people)
 		for i, _ := range peopleMap {
 			for ii, vv := range pairs {
-				fmt.Println("Day", day)
+				//fmt.Println("Day", day)
 				if vv.scheduled == false && (i == vv.name1 || i == vv.name2) && (peopleMap[vv.name1] == false && peopleMap[vv.name2] == false) {
 					pairs[ii].day = day
 					pairs[ii].scheduled = true
@@ -67,8 +79,8 @@ func scheduleDay(pairs []pair, people []string) []pair {
 					peopleMap[vv.name2] = true
 					pairsScheduled = pairsScheduled + 1
 				}
-				fmt.Printf("index:%5v\tv.name1:%8v\tv.name2:%8v\tv.day:%4v\tv.scheduled:%8v\n", i, pairs[ii].name1, pairs[ii].name2, pairs[ii].day, pairs[ii].scheduled)
-				fmt.Println(peopleMap)
+				//fmt.Printf("index:%5v\tv.name1:%8v\tv.name2:%8v\tv.day:%4v\tv.scheduled:%8v\n", i, pairs[ii].name1, pairs[ii].name2, pairs[ii].day, pairs[ii].scheduled)
+				//fmt.Println(peopleMap)
 			}
 		}
 		day++
